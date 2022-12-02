cec2f4ab6a185dc6c7c7624d9b86f98aa16529c1
Ademar Madeira Junior <ademar.junior@newfold.com>
2022-12-02 13:25:51 -0300

    0.0.11

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
   devtool: "inline-source-map",
   entry: "./src/index.tsx",
   output: {
-    // path: path.resolve("build"),
+    path: path.resolve("build"),
     filename: "index.ts",
   },
   module: {
