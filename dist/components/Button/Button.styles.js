4850c9d20bbebf14af243d34e49e8185b382a96f
Ademar Madeira Junior <ademar.junior@newfold.com>
2022-12-02 14:02:58 -0300

    0.0.23

--- dist/components/Button/Button.styles.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Button = void 0;
-const react_1 = require("@stitches/react");
-exports.Button = (0, react_1.styled)('button', {
-    backgroundColor: '$blue',
-    borderRadius: '$defaultRadius',
-});
--- dist/styles/externals/index.js
+++
@@ -1,2 +0,0 @@
-"use strict";
-// import 'react-loading-skeleton/dist/skeleton.css';
