1e1c8ef17008ac150290861816cde449c10811a3
Ademar Madeira Junior <ademar.junior@newfold.com>
2022-12-02 13:48:10 -0300

    0.0.19

---
+++ dist/components/Button/Button.styles.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Button = void 0;
+const react_1 = require("@stitches/react");
+exports.Button = (0, react_1.styled)('button', {
+    backgroundColor: '$blue',
+    borderRadius: '$defaultRadius',
+});
---
+++ dist/styles/generics/resetCSS.js
@@ -0,0 +1,27 @@
+"use strict";
+// import { ResetCSS } from '@/types/types';
+// const resetCSS: ResetCSS = {
+//   '*': {
+//     margin: 0,
+//     padding: 0,
+//     listStyle: 'none',
+//     boxSizing: 'border-box',
+//     '&:after, &:before': {
+//       boxSizing: 'border-box',
+//     },
+//   },
+//   img: {
+//     display: 'block',
+//     maxWidth: '100%',
+//   },
+//   input: {
+//     '&::-webkit-outer-spin-button, &::-webkit-inner-spin-button': {
+//       WebkitAppearance: 'none',
+//       margin: 0,
+//     },
+//     '&[type=number]': {
+//       MozAppearance: 'textfield',
+//     },
+//   },
+// };
+// export default resetCSS;
